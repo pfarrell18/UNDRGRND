0270d37e4b022ee86c0567658f6731ef29e8d5f9
rcollins22 <63428010+rcollins22@users.noreply.github.com>
2020-07-07 16:31:26 -0400

    Revert "api"

--- src/getAPI.js
+++ src/getAPI.js
@@ -1,10 +1,9 @@
-// const Handlebars = require(['handlebars'])
-
-const getAPI = function(callout) {
+const Handlebars = require('requirejs')
+(function() {
     // GENERATES AUTH LINK TO GIVE UNDRGRND USER PERMISSIONS
     function login(callback) {
-        let CLIENT_ID = '933790dc3b564fa6b2ed3d8f09ed440f';
-        let REDIRECT_URI = 'https://1undrgrnd.s3.amazonaws.com/index.html';
+        var CLIENT_ID = '933790dc3b564fa6b2ed3d8f09ed440f';
+        var REDIRECT_URI = 'https://1undrgrnd.s3.amazonaws.com/frontend/trial.html';
         function getLoginURL(scopes) {
             return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
               '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
@@ -13,60 +12,63 @@ const getAPI = function(callout) {
         }
         
         // SCOPES (PERMISSIONS) USED TO GENERATE KEY
-        let url = getLoginURL([
+        var url = getLoginURL([
             'user-read-email user-follow-modify user-top-read user-library-modify user-follow-modify'
         ]);
         
         // DISPLAY SETTINGS FOR POPUP AUTH BOX
-        let width = 450,
+        var width = 450,
             height = 730,
             left = (screen.width / 2) - (width / 2),
             top = (screen.height / 2) - (height / 2);
         // PARSES DATA AND CALLS BACK TOKEN HASH
         window.addEventListener("message", function(event) {
-            let hash = JSON.parse(event.data);
-            console.dir(hash)
+            var hash = JSON.parse(event.data);
             if (hash.type == 'access_token') {
-                uToken = callback(hash.access_token);  
+                callback(hash.access_token);
             }
         }, false);
         
-        let w = window.open(url,'Spotify',
-            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
-        );
+        var w = window.open(url,
+                            'Spotify',
+                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+                           );
         
     }
- 
+    // FETCHES USER INFO FROM 'me' API
     function getUserData(accessToken) {
-         fetch(
-        'https://api.spotify.com/v1/me',
-            {headers: {
+         $.ajax({
+            url: 'https://api.spotify.com/v1/me',
+            headers: {
                'Authorization': 'Bearer ' + accessToken
-            }  
-        }).then(response=> response.json()).then(response=>{ 
-            console.dir(response)
-            callout(accessToken,response)
-            // THIS IS WHERE I NEED TO EXIT AND DO SOMETHING
-        }
-        ); 
+            }
+        });
         
     }
 
-    // console.log(uToken)
+    // function getFavArtists(accessToken) {
+    //     return $.ajax({
+    //         url: 
+    //     })
+    // }
 
-    let loginButton = document.getElementById('btn-login');
-
-
+    // THIS FUNCTION JUST PUSHES THE USERS INFO TO THE DOM, NOT NECESSARY AT ALL
+    var templateSource = document.getElementById('result-template').innerHTML,
+        template = Handlebars.compile(templateSource),
+        resultsPlaceholder = document.getElementById('result'),
+        loginButton = document.getElementById('btn-login');
     
     loginButton.addEventListener('click', function() {
         login(function(accessToken) {
             getUserData(accessToken)
-            return accessToken;
-                
+                .then(function(response) {
+                    loginButton.style.display = 'none';
+                    resultsPlaceholder.innerHTML = template(response);
+    //                 
+                });
             });
     });
     
-};
+})();
 
 
-// console.log(uToken)
