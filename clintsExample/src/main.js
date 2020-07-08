0ae7ff7e9600d4398661bd37a8e34ab8237c3888
Clint Fleetwood <clint@digitalcrafts.com>
2020-07-07 22:51:43 -0400

    added things

---
+++ clintsExample/src/main.js
@@ -0,0 +1,27 @@
+import authenticate from "../libs/authenticate.js";
+
+let root = document.querySelector('#content');
+
+let token = localStorage.getItem('access_token');
+let expires_on = localStorage.getItem('expires_on');//expires on is when the token expires as a timestamp
+
+if(!token || token === 'undefined' || expires_on === 'NaN' || Number(expires_on) < new Date().getTime()){
+    let loginButton = document.createElement('button');
+    loginButton.innerHTML ='Click Here to Login into Spotify';
+    loginButton.addEventListener('click', authenticate);
+    root.append(loginButton);
+} else {
+
+//Here you can start you app with fetch and all that using the token etc
+//you have to enable this chrome extension for it to work which you cannot expect users to do.
+//https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en
+    fetch(`https://api.spotify.com/v1/me`, {
+      headers:{
+          'Authorization':`Bearer ${token}`
+      }  
+    })
+    .then(resp=>resp.json())
+    .then(json=>console.log(json))
+    .catch(error=>console.log(error))
+
+}
