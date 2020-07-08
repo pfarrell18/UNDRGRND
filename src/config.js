6e5d7d00e61410fd1b9eeaff083a0558492602ec
Rashad Collins <rashad.collins22@gmail.com>
2020-07-08 14:30:28 -0400

    updated index

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const clientId = '933790dc3b564fa6b2ed3d8f09ed440f';//the clientId of the app
-export const redirectLogin = "http://127.0.0.1:5500/acceptLogin=true";//needs to the the url you set in spotify just with the ?acceptLogin=true
+export const redirectLogin = "http://127.0.0.1:5500/?acceptLogin=true";//needs to the the url you set in spotify just with the ?acceptLogin=true
 export const redirectComplete = "http://127.0.0.1:5500/callback/"
