48e1c36507a7d8f7d0b9acc052662f2ab866545f
Rashad Collins <rashad.collins22@gmail.com>
2020-07-10 20:18:27 -0400

    UTD

--- src/getArtists.js
+++ src/getArtists.js
@@ -8,14 +8,12 @@ export async function getTopArtist(){
     const asJson = await response.json();
     console.log(asJson)
     const artistids = [asJson.items[0].id, asJson.items[1].id, asJson.items[2].id, asJson.items[3].id, asJson.items[4].id]
-    console.log(artistids)
     return artistids;
 }
     
 export async function getSimiliarArtist(seedartists){
     const response = await fetch(`https://api.spotify.com/v1/recommendations?seed_artists=${seedartists[0]}%2C${seedartists[1]}%2C${seedartists[2]}%2C${seedartists[3]}%2C${seedartists[4]}&max_popularity=30&limit=100`, requestOptions)
     const asJson = await response.json();
-    console.log(asJson)
     //runs both functions,
     checkRepeatingArtist(asJson,seedartists)
     .then(r=> {
@@ -25,23 +23,29 @@ export async function getSimiliarArtist(seedartists){
     })
     
     .then(re=>{ console.log(re);
-        checkPopularity(re)})
-    // .then(generateArtistImages);
-    
-
-    // .then(resp=>{ console.dir(resp)})
+       return checkPopularity(re)})
+    .then(res=> {
+        boone=generateArtistImages(res);
+        console.log(boone);
+        return boone;
+    })
+    .then(function(final){
+        return final;
+    })
     // // .then(rsp=>{
     // //     for (var i=0; i<rsp.length; i++){
     // //         rsp[i].addEventListener("click", showGoal)
     // //     }
     // // })
+   
+    // .catch(error=> {console.log(error)});
 }
 
-export async function main(){
-    const seedartists = await getTopArtist();
-    const similiarArtist = await getSimiliarArtist(seedartists);
-    console.log(seedartists)
-}
+// export async function main(){
+//     const seedartists = await getTopArtist();
+//     const similiarArtist = await getSimiliarArtist(seedartists);
+//     console.log(seedartists)
+// }
 
-//this initiates all functions running
-main()
+// //this initiates all functions running
+// main()
