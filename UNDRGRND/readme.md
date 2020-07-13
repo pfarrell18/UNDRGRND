3ada1628a0c8fc77c1c42192089c79794294e1dd
Priyanka Farrell <66265731+pfarrell18@users.noreply.github.com>
2020-07-13 15:06:31 -0400

    Add files via upload

---
+++ UNDRGRND/readme.md
@@ -0,0 +1,18 @@
+Have you ever found it difficult to find new music? Have you ever found it difficult finding music by local and underground music? Have you ever found it difficult sharing music?  
+
+Well look no farther. Welcome to UNDRGRND, a one of its kind music app created by Rashad Collins, Ivan Davis, and Priyanka Farrell.
+
+With UNDRGRND, users will be prompted to login and authorize our app to access their spotify data. Using a series of spotify apis (listed below), our app will generate a list of UNDRGRND (<250,000 followers) artists based on user preferences. the app will provide the image, name, follower count, and an QR code linking to the artist's spotify page. 
+
+After the app generates UNDRGRND artists, it will also give users the ability to find more UNDRGRND artists based on the genre of their choice. If you are someone who loves surprises, unique music finds or  are a supporter of local artists, this is the app for you!
+
+APIS used: 
+- Spotify Get Recommendations Based on Seeds
+- Spotify Get Users Top Artists and Tracks
+- Spotify Get an Artist
+- GO.me QR Code Generator
+
+
+
+
+
---
+++ UNDRGRND/src/searchArtists.js
@@ -0,0 +1,37 @@
+import { requestOptions } from "./initialfetch.js";
+import {checkRepeatingArtist, checkPopularity, generateArtistImages,checkIfUserFollows} from "./calls.js";
+import {getSimiliarArtist,getTopArtist} from "./getArtists.js"
+
+async function main(){
+    console.log("main running")
+    const seedartists = await getTopArtist();
+    const similiarArtist = await getSimiliarArtist(seedartists);
+}
+main()
+
+const form = document.getElementsByClassName("form")
+const genreSearch = document.createElement("h3")
+genreSearch.innerHTML = "Want more? Use our genre search!"
+const genrebutton = document.createElement("button")
+const linkgenresearch = document.createElement("a")
+linkgenresearch.setAttribute("href", "genre.html")
+linkgenresearch.innerHTML = "GENRE SEARCH"
+genrebutton.append(linkgenresearch)
+console.log(genreSearch)
+form[0].append(genreSearch, genrebutton)
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
