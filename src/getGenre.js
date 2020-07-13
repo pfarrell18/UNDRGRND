b62ba101f7af7f6330bcdd93dcee445b1ab8dbf2
Priyanka Farrell <66265731+pfarrell18@users.noreply.github.com>
2020-07-13 11:23:24 -0400

    Update getGenre.js

--- src/getGenre.js
+++ src/getGenre.js
@@ -49,3 +49,19 @@ export function showGoal(){
 this.children[0].classList.toggle("hidden")
 this.children[1].classList.toggle("hidden")
 }
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
